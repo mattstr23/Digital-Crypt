e852a9dfbe0f24fa960f5f5de2da7f709de92dcc
Matthew Valicoff <matthewvalicoff@Matthews-MacBook-Pro.local>
2021-11-01 09:06:25 -0500

    fixed reducer breaking

--- react-project/src/redux/reducers/rootReducer.js
+++ react-project/src/redux/reducers/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 import marketData from "./MarketReducer";
-import coinInfo from "./CoinInfoReducer";
 
 const rootReducer = combineReducers({
     marketData,
-    coinInfo,
 })
 export default rootReducer
